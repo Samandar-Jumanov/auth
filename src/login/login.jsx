1da67679d8fae184b1d9b389595d5f4559755718
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-03-04 23:44:12 +0300

    'updated'

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,20 +3,33 @@ import './login.css'
 import { AuthContext } from '../helpers/Context'
 const Login = ()=>{
   const {setPage} = useContext(AuthContext)
+   const [email , setEmail] = useState('')
+   const [password , setPassword] = useState('')
+   const [alert, setAlert] = useState(false)
 
-
-
+const getAuth = ()=>{
+  if(email===''|| email<0 || !email.includes('@')|| email===Number &&
+   password ===''|| password <0||password>20 
+  ){
+     setAlert(true)
+  }
+}
    return (
      <div className='Login'>
+      {alert ? <h1 className='header' style={{color:'red'}}>Something went wrong</h1> : ''}
         <div className="innerDiv">
           <h1 className="header">Sign in </h1>
-          <input type="text" className='box email'placeholder='username or email...' />
-          <input type="text" className='box password' placeholder='password...' />
-          <button className='button in'>Sign in </button>
+          <input type="text" className='box email'placeholder='username or email...' 
+          onChange={e=>setEmail(e.target.value)}
+          />
+          <input type="text" className='box password' placeholder='password...' 
+          onChange={e=>setPassword(e.target.value)}
+          />
+          <button className='button in' onClick={getAuth}>Sign in </button>
           <p>Dont have an account</p>
           <button
            onClick={()=>setPage('up')}
-            className='button up'
+          className='button up'
            >Signup</button>
         </div>
      </div>
